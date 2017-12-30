0e441ee0e54e08de7ff7ce26cef2988eae23f5cc
neekey <ni184775761@gmail.com>
2017-12-30 16:16:02 +1100

    add socket io

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/test', (req, res) => res.send('test'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+io.on('connection', function(socket){
+    console.log('a user connected');
+});
+
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('pages/index'));
+app.all('/webhook', (req, res) => {
+  const requestData = {
+    body: req.body,
+    hostname: req.hostname,
+    method: req.method,
+    path: req.path,
+    query: req.query,
+  };
+  console.log(requestData);
+  io.emit('received', requestData);
+  res.sendStatus(200);
+});
+
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`));
